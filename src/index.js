bca010c03a0071bdfd7551bcc8b3b70651f3df48
malfell <malfellows13@gmail.com>
2022-07-26 14:47:09 -0500

    enabled concurrent mode

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// enables Concurrent Mode in React
+// .createRoot instead of .render lets you access new, experimental features in React
+ReactDOM.createRoot(document.getElementById('root').render(<App />))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
